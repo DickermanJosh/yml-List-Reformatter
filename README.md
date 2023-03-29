66cd56d12c0e185e2ab78f05b7a5c8918d43e6ef
DickermanJosh <dickermanjosh@gmail.com>
2023-03-28 18:50:24 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # yml-List-Reformatter
- This is a simple project made to reformat large lists of text by adding "  - " before every line. I created this simple script to avoid having to manually add a hyphen and the proper spacing before every element of a list in a YML file.
+ This is a simple project made to reformat large lists of text by adding "  - " before every line. 
+ I created this simple script to avoid having to manually add a hyphen and the proper spacing before 
+ every element of a list in a YML file.
+
+
+ EXAMPLE:
+ A
+ B
+ C
+ Becomes ->
+   - A
+   - B
+   - C
+ USAGE:
+   - Copy the contents of your list into RawList.txt OR import your file into the project src
+   - After loading the script in a terminal or command window, enter the content root path of the file 
+     you wish to reformat into the scanner and press enter
+   - Your reformatted file will be updated under ReformattedList.txt, your original file will not be altered in any way
---
+++ src/Reformatter.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+    This is a simple script to format lists to include two spaces and a hyphen before each element of the list, developed for moving large lists into yml files
+    EXAMPLE:
+    A
+    B
+    C
+    Becomes ->
+      - A
+      - B
+      - C
+    USAGE:
+     - Copy the contents of your list into RawList.txt OR import your file into the project src
+     - After loading the script in a terminal or command window, enter the content root path of the file you wish to reformat into the scanner and press enter
+     - Your reformatted file will be updated under ReformattedList.txt, your original file will not be altered in any way
+ */
+
+public class Reformatter {
+    public static void main(String[] args) {
+
+        System.out.print("Enter the root path of the file you want to reformat: ");
+
+        Scanner scan = new Scanner(System.in);
+        String rawListPath = scan.nextLine().toString();
+
+        ReadFromFile(rawListPath);
+    }
+
+    private static void ReadFromFile(String filename){
+        File fileToConvert = new File(filename);
+        List lines = new ArrayList<String>();
+        try {
+            Scanner scan = new Scanner(fileToConvert);
+
+            while(scan.hasNextLine()){
+                lines.add(scan.nextLine().toString());
+            }
+
+            ReformatFile(lines);
+
+        }catch(FileNotFoundException e){
+            System.out.println(filename + " could not be opened, bad pathway");
+        }
+    }
+    private static void ReformatFile(List rawText){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("ReformattedList.txt"))) {
+            for(int i = 0; i < rawText.size(); i++){
+                writer.write("  - " + rawText.get(i) + "\n");
+            }
+            System.out.println("File converted successfully as ReformattedList.txt");
+        }catch(IOException e){
+            System.out.println("Could not reformat file");
+        }
+    }
+}
